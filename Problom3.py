167bda90602ddedebae7d29e6578a5b53f431fa3
Vitaly01 <sashaomg@hotmail.com>
2015-07-22 13:39:59 -0400

    Problom 3

---
+++ Problom3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Problom Number 3
+
+number = 600851475143
+i = 2
+
+while not(i > number):
+    answer = number % i
+    if answer == 0:
+        print i
+        number = number / i
+        i = i + 1
+        continue
+    elif answer != 0:
+        i = i + 1
+        continue
